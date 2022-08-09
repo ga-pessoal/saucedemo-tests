45034600cba6cd7554a3b0af0be79e7d5d47e5fd
Gabriel Costa <gabriel.mattos@stoque.com.br>
2022-08-09 00:47:26 -0300

    Add: - Reports

--- cypress.config.js
+++ cypress.config.js
@@ -1,12 +1,20 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  reporter: 'cypress-mochawesome-reporter',
+  reporterOptions: {
+    charts: true,
+    reportPageTitle: 'saucedemo tests',
+    embeddedScreenshots: true,
+    inlineAssets: true,
+    saveAllAttempts: false,
+  },
   e2e: {
     setupNodeEvents(on, config) {
-      
+      require('cypress-mochawesome-reporter/plugin')(on);
     },
     baseUrl: 'https://www.saucedemo.com/',
     chromeWebSecurity: false,
-    video:false
+    video:false,
   },
 });
